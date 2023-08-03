1a608ea72ca7caf68d4f1a4369c13c3760e8996f
josugoar <jogoar@opendeusto.es>
2023-08-03 14:43:52 +0200

    Remove unnecessary smart pointers (for the better)

--- include/csp.h
+++ include/csp.h
@@ -1,12 +1,12 @@
 #ifndef CSP_H
 #define CSP_H
 
-#ifndef __STDC_NO_ATOMICS__
+#ifdef __STDC_NO_ATOMICS__
+#warning "atomic pointers require stdatomic.h"
+#else
 #include <csp/atomic_shared_ptr.h>
 #include <csp/atomic_weak_ptr.h>
 #endif
-#include <csp/local_shared_ptr.h>
-#include <csp/scoped_ptr.h>
 #include <csp/shared_ptr.h>
 #include <csp/unique_ptr.h>
 #include <csp/weak_ptr.h>
