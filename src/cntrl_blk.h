1983663ee7d5318029a73ed4c30821d42cecef72
josugoar <jogoar@opendeusto.es>
2023-08-03 14:47:52 +0200

    Start fixing the control block and finish project

--- src/cntrl_blk.h
+++ src/cntrl_blk.h
@@ -1,13 +1,19 @@
 #ifndef CSP_CNTRL_BLK_H
 #define CSP_CNTRL_BLK_H
 
+#ifdef __STD_NO_ATOMICS__
+#warning "shared pointers require stdatomic.h for synchronization"
+#else
+#include <stdatomic.h>
+#endif
+
 typedef void csp_cntrl_blk_T;
 
 typedef void (*csp_cntrl_blk_D)(csp_cntrl_blk_T *);
 
 typedef struct csp_cntrl_blk csp_cntrl_blk;
 
-void csp_cntrl_blk_init(csp_cntrl_blk *_this, csp_cntrl_blk_T *_p, csp_cntrl_blk_D _d);
+csp_cntrl_blk *csp_cntrl_blk_pd(csp_cntrl_blk *_this, csp_cntrl_blk_T *_p, csp_cntrl_blk_D _d);
 
 csp_cntrl_blk_D *csp_cntrl_blk_get_deleter(csp_cntrl_blk *_this);
 
@@ -23,8 +29,18 @@ void csp_cntrl_blk_add_weak(csp_cntrl_blk *_this);
 
 void csp_cntrl_blk_release_weak(csp_cntrl_blk *_this);
 
+#ifdef __STD_NO_ATOMICS__
+#define CSP_ATOMIC_LONG long
+#else
+#define CSP_ATOMIC_LONG atomic_long
+#endif
+
 struct csp_cntrl_blk
 {
+    CSP_ATOMIC_LONG _shared_owners;
+    CSP_ATOMIC_LONG _weak_owners;
+    csp_cntrl_blk_T *_p;
+    csp_cntrl_blk_D _d;
 };
 
 #endif
