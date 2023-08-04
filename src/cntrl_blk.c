9fa6b343592f4e3657373044e059c8822ffd1312
Josu <josugoar@gmail.com>
2023-08-04 09:38:41 +0200

    Add fuction prototypes

--- src/cntrl_blk.c
+++ src/cntrl_blk.c
@@ -3,68 +3,17 @@
 #include <assert.h>
 #include <stdlib.h>
 
-static inline long csp_cntrl_blk_load(const CSP_CNTRL_BLK_LONG *const _value)
-{
-#ifdef __STD_NO_ATOMICS__
-    return *_value;
-#else
-    return atomic_load(_value);
-#endif
-}
+static inline long csp_cntrl_blk_load(const CSP_CNTRL_BLK_LONG *_value);
 
-static inline long csp_cntrl_blk_load_relaxed(const CSP_CNTRL_BLK_LONG *const _value)
-{
-#ifdef __STD_NO_ATOMICS__
-    return *_value;
-#else
-    return atomic_load_explicit(_value, memory_order_relaxed);
-#endif
-}
+static inline long csp_cntrl_blk_load_relaxed(const CSP_CNTRL_BLK_LONG *_value);
 
-static inline long csp_cntrl_blk_load_aquire(const CSP_CNTRL_BLK_LONG *const _value)
-{
-#ifdef __STD_NO_ATOMICS__
-    return *_value;
-#else
-    return atomic_load_explicit(_value, memory_order_acquire);
-#endif
-}
+static inline long csp_cntrl_blk_load_aquire(const CSP_CNTRL_BLK_LONG *_value);
 
-static inline long csp_cntrl_blk_add_fetch_relaxed(CSP_CNTRL_BLK_LONG *const _value)
-{
-#ifdef __STD_NO_ATOMICS__
-    return ++(*_value);
-#else
-    return atomic_fetch_add_explicit(_value, 1, memory_order_relaxed) + 1;
-#endif
-}
+static inline long csp_cntrl_blk_add_fetch_relaxed(CSP_CNTRL_BLK_LONG *_value);
 
-static inline long csp_cntrl_blk_sub_fetch_relaxed(CSP_CNTRL_BLK_LONG *const _value)
-{
-#ifdef __STD_NO_ATOMICS__
-    return --(*_value);
-#else
-    return atomic_fetch_sub_explicit(_value, 1, memory_order_relaxed) + 1;
-#endif
-}
+static inline long csp_cntrl_blk_sub_fetch_relaxed(CSP_CNTRL_BLK_LONG *_value);
 
-static inline bool csp_cntrl_blk_compare_exchange_weak(CSP_CNTRL_BLK_LONG *const _value, long *_expected, const long _desired)
-{
-#ifdef __STD_NO_ATOMICS__
-    if (*_value == *_expected)
-    {
-        *_value = _desired;
-
-        return true;
-    }
-
-    *_expected = *_value;
-
-    return false;
-#else
-    return atomic_compare_exchange_weak(_value, _expected, _desired);
-#endif
-}
+static inline bool csp_cntrl_blk_compare_exchange_weak(CSP_CNTRL_BLK_LONG *_value, long *_expected, long _desired);
 
 csp_cntrl_blk *csp_cntrl_blk_pd(csp_cntrl_blk *const _this, csp_cntrl_blk_T *const _p, const csp_cntrl_blk_D _d)
 {
@@ -144,4 +93,67 @@ void csp_cntrl_blk_release_weak(csp_cntrl_blk *const _this)
     {
         free(_this);
     }
+}
+
+long csp_cntrl_blk_load(const CSP_CNTRL_BLK_LONG *const _value)
+{
+#ifdef __STD_NO_ATOMICS__
+    return *_value;
+#else
+    return atomic_load(_value);
+#endif
+}
+
+long csp_cntrl_blk_load_relaxed(const CSP_CNTRL_BLK_LONG *const _value)
+{
+#ifdef __STD_NO_ATOMICS__
+    return *_value;
+#else
+    return atomic_load_explicit(_value, memory_order_relaxed);
+#endif
+}
+
+long csp_cntrl_blk_load_aquire(const CSP_CNTRL_BLK_LONG *const _value)
+{
+#ifdef __STD_NO_ATOMICS__
+    return *_value;
+#else
+    return atomic_load_explicit(_value, memory_order_acquire);
+#endif
+}
+
+long csp_cntrl_blk_add_fetch_relaxed(CSP_CNTRL_BLK_LONG *const _value)
+{
+#ifdef __STD_NO_ATOMICS__
+    return ++(*_value);
+#else
+    return atomic_fetch_add_explicit(_value, 1, memory_order_relaxed) + 1;
+#endif
+}
+
+long csp_cntrl_blk_sub_fetch_relaxed(CSP_CNTRL_BLK_LONG *const _value)
+{
+#ifdef __STD_NO_ATOMICS__
+    return --(*_value);
+#else
+    return atomic_fetch_sub_explicit(_value, 1, memory_order_relaxed) + 1;
+#endif
+}
+
+bool csp_cntrl_blk_compare_exchange_weak(CSP_CNTRL_BLK_LONG *const _value, long *const _expected, const long _desired)
+{
+#ifdef __STD_NO_ATOMICS__
+    if (*_value == *_expected)
+    {
+        *_value = _desired;
+
+        return true;
+    }
+
+    *_expected = *_value;
+
+    return false;
+#else
+    return atomic_compare_exchange_weak(_value, _expected, _desired);
+#endif
 }
